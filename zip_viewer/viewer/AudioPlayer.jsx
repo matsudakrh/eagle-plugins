bb59f61a0605347a71b2df4a9a03976494dd97e1
Hiroki Matsuda <matsuda.ddcm@gmail.com>
2024-12-14 15:52:01 +0900

    AudioPlayerにサムネイルを追加

--- zip_viewer/viewer/AudioPlayer.jsx
+++ zip_viewer/viewer/AudioPlayer.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const AudioPlayer = ({ entry }) => {
+  const audio = useRef(null)
   const [src, setSrc] = useState()
+  const [thumb, setThumb] = useState()
 
   useEffect(() => {
     if (!entry) {
@@ -28,8 +30,32 @@ const AudioPlayer = ({ entry }) => {
     }
   }, [entry])
 
-  return <div style={{ display: 'flex', alignItems: 'end', height: '100%' }}>
-    <audio src={src} style={{ width: '100%' }} controls autoPlay></audio>
+  useEffect(() => {
+    const getThumbnail = async () => {
+      let item = (await window.eagle.item.getSelected())[0]
+      setThumb(item.thumbnailPath)
+    }
+
+    getThumbnail().catch(() => {})
+  }, [])
+
+  const handleClick = () => {
+    if (!audio.current) {
+      return
+    }
+
+    if (audio.current.paused) {
+      audio.current.play()
+    } else {
+      audio.current.pause()
+    }
+  }
+
+  return <div style={{ display: 'grid', gridTemplateRows: '1fr auto', height: '100%' }}>
+    <div onClick={handleClick} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <img src={thumb} alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+    </div>
+    <audio ref={audio} src={src} style={{ width: '100%' }} controls autoPlay></audio>
   </div>
 }
 window.components.AudioPlayer = AudioPlayer
