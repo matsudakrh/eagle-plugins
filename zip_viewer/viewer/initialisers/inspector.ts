d717b0816aac4f9330654db644cc2afc3861bdf6
Hiroki Matsuda <matsuda.ddcm@gmail.com>
2025-01-15 01:36:45 +0900

    最後に開いたファイルをリアルタイムに更新

--- zip_viewer/viewer/initialisers/inspector.ts
+++ zip_viewer/viewer/initialisers/inspector.ts
@@ -3,26 +3,30 @@ import { DBConfig } from '../db/config'
 import AppParameters from '../lib/app-parameters'
 
 window.eagle.onPluginCreate(() => {
-  let db: IDBDatabase
-  const openReq = indexedDB.open(AppParameters.pluginId, DBConfig.VERSION)
+  const renderLastFile = () => {
+    let db: IDBDatabase
+    const openReq = indexedDB.open(AppParameters.pluginId, DBConfig.VERSION)
 
-  openReq.onsuccess = (event) => {
-    db = (event.target as IDBOpenDBRequest).result
-    if (!db.objectStoreNames.contains(DBConfig.STORE_NAMES.Info)) {
-      return
-    }
+    openReq.onsuccess = (event) => {
+      db = (event.target as IDBOpenDBRequest).result
+      if (!db.objectStoreNames.contains(DBConfig.STORE_NAMES.Info)) {
+        return
+      }
 
-    const transaction = db.transaction(DBConfig.STORE_NAMES.Info, 'readonly')
-    const store = transaction.objectStore(DBConfig.STORE_NAMES.Info)
-    const getReq = store.get([AppParameters.paramsId])
+      const transaction = db.transaction(DBConfig.STORE_NAMES.Info, 'readonly')
+      const store = transaction.objectStore(DBConfig.STORE_NAMES.Info)
+      const getReq = store.get([AppParameters.paramsId])
 
-    getReq.onsuccess = () => {
-      if (getReq.result) {
-        const p = document.getElementById('lastFileName')
-        if (p) {
-          p.textContent =  getReq.result.lastFilePath
+      getReq.onsuccess = () => {
+        if (getReq.result) {
+          const p = document.getElementById('lastFileName')
+          if (p) {
+            p.textContent =  getReq.result.lastFilePath.split('/').join('/\n')
+          }
         }
       }
     }
   }
+  renderLastFile()
+  setInterval(renderLastFile, 1000)
 })
