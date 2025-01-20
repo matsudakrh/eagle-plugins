de98ae8eb931a4b088aca200e503c034d68fecb1
Hiroki Matsuda <matsuda.ddcm@gmail.com>
2025-01-20 23:13:40 +0900

    サムネイルの設定は非同期にする

--- zip_viewer/viewer/lib/app-metadata.ts
+++ zip_viewer/viewer/lib/app-metadata.ts
@@ -2,11 +2,13 @@ import fs from 'fs'
 import AppParameters from '../lib/app-parameters'
 
 export default class AppMetadata {
-  static async setThumbnail(data: { src: string, key: string }) {
-    const metadata = AppParameters.metadata
-    metadata.thumbnails ||= {}
-    metadata.thumbnails[data.key] = data.src
-    fs.writeFileSync(AppParameters.metadataFilePath, JSON.stringify(metadata), 'utf8')
+  static async setThumbnail(data: { src: string, key: string }): Promise<void> {
+    // TODO: indexedDBの方が良い？要検討の為一旦設定機能を止めておく
+    // TODO: あるいはthumbnails.jsonを作成してしまう・コマンドでの削除を前提とするならこちらのほうが捗りそう
+    // const metadata = AppParameters.metadata
+    // metadata.thumbnails ||= {}
+    // metadata.thumbnails[data.key] = data.src
+    // return fs.promises.writeFile(AppParameters.metadataFilePath, JSON.stringify(metadata), 'utf8')
   }
 
   static getThumbnail(key: string) {
